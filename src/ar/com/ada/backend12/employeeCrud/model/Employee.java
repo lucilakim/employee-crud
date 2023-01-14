6091d29eceef710e0d94d9bdab1faff57e3503f1
Lucila Kim <lucila.kim.9@gmail.com>
2023-01-14 19:56:10 -0300

    Add the employee class with its attributes and methods

---
+++ src/ar/com/ada/backend12/employeeCrud/model/Employee.java
@@ -0,0 +1,123 @@
+package ar.com.ada.backend12.employeeCrud.model;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Objects;
+
+public class Employee {
+    // Attributes
+    int id;
+    String firstName;
+    String lastName;
+    int di;
+    Date birthDate;
+    String department;
+    Timestamp hiringDate;
+    int salary;
+
+    // Constructor
+    public Employee(int id, String firstName, String lastName, int di, Date birthDate, String department, Timestamp hiringDate, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.di = di;
+        this.birthDate = birthDate;
+        this.department = department;
+        this.hiringDate = hiringDate;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getDi() {
+        return di;
+    }
+
+    public void setDi(int di) {
+        this.di = di;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Timestamp getHiringDate() {
+        return hiringDate;
+    }
+
+    public void setHiringDate(Timestamp hiringDate) {
+        this.hiringDate = hiringDate;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    // Equals - HashCode - ToString
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && di == employee.di && salary == employee.salary && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(birthDate, employee.birthDate) && Objects.equals(department, employee.department) && Objects.equals(hiringDate, employee.hiringDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, di, birthDate, department, hiringDate, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "ar.com.ada.backend12.employeeCrud.model.Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", di=" + di +
+                ", birthDate=" + birthDate +
+                ", department='" + department + '\'' +
+                ", hiringDate=" + hiringDate +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
