4c041e221ad68b3f8db64839d4b523a04f850671
Lucila Kim <lucila.kim.9@gmail.com>
2023-01-16 16:16:22 -0300

    Adds the functionality of updating employee ONLY IN THE DATABASE.

--- src/ar/com/ada/backend12/employeeCrud/model/HandlerOptions.java
+++ src/ar/com/ada/backend12/employeeCrud/model/HandlerOptions.java
@@ -8,7 +8,6 @@ public class HandlerOptions {
 
     public int insertEmployee(Connection conn, String firstName, String lastName, String di, java.util.Date birthDate, int department, int salary) {
         int id = 0;
-
         //2 - Prepare our query, be it select, delete, etc.
         String sql = "INSERT INTO EMPLOYEE (FIRST_NAME, LAST_NAME, DI, BIRTH_DATE, DEPARTMENT, SALARY) VALUES (?,?,?,?,?,?)";
         PreparedStatement stmt = null;
@@ -28,13 +27,11 @@ public class HandlerOptions {
             if (result.next()) {
                 id = result.getInt(1);
             }
-
-            if (stmt != null) { try { stmt.close();} catch (SQLException f) { f.printStackTrace(); }}
-
             return id;
-
         } catch (SQLException e) {
             return -1;
+        } finally {
+            if (stmt != null) { try { stmt.close();} catch (SQLException f) { f.printStackTrace(); }}
         }
     }
 
@@ -86,5 +83,32 @@ public class HandlerOptions {
             System.out.println(id + ", '" + name + "', '" + lastName + "', '" + di + "', '" + birth + "', " + department + ", '" + hiringDate + "', $" + salary + ";");
         }
         if (stmt != null) { try { stmt.close();} catch (SQLException f) { f.printStackTrace(); }}
+    }
+
+    public int updateEmployee(Connection conn, String firstName, String lastName, String di, java.util.Date birthDate, int department, int salary, int id) throws SQLException {
+        //2 - Prepare our query, be it select, delete, etc.
+        String sql = "UPDATE EMPLOYEE SET FIRST_NAME = ?, LAST_NAME = ? , DI = ?, BIRTH_DATE = ?, DEPARTMENT = ?, SALARY = ? WHERE ID = ?";
+        PreparedStatement stmt = null;
+        try {
+            stmt = conn.prepareStatement(sql);
+            stmt.setString(1, firstName);
+            stmt.setString(2, lastName);
+            stmt.setString(3, di);
+            stmt.setDate(4, new java.sql.Date(birthDate.getTime()));
+            stmt.setInt(5, department);
+            stmt.setInt(6, salary);
+            stmt.setInt(7, id);
+
+            //3 - Execute our query
+            stmt.executeUpdate();
+            return 1;
+
+        } catch (SQLException e) {
+            System.out.println("ERROR! An error occurred while trying to update the user.");
+        return -1;
+
+    } finally {
+        if (stmt != null) { try { stmt.close();} catch (SQLException f) { f.printStackTrace(); }}
+    }
     }
 }
