5c42bb25469fd24bbe8f9354cad16efbfe9c5f0a
Lucila Kim <lucila.kim.9@gmail.com>
2023-01-16 12:28:08 -0300

    Add the menu with the list of Departments

--- src/ar/com/ada/backend12/employeeCrud/model/Menu.java
+++ src/ar/com/ada/backend12/employeeCrud/model/Menu.java
@@ -18,4 +18,26 @@ public class Menu {
                 "    ----------------------------------------- \n";
         System.out.println(menu);
     }
+
+    public void printDepartmentMenu() {
+        String departmentMenu = " >>> SELECT EMPLOYEE DEPARTMENTS \n" +
+                "    1 - SYSTEMS,\n" +
+                "    2 - HUMAN_RESOURCES,\n" +
+                "    3 - ACCOUNTING,\n" +
+                "    4 - CUSTOMER_SERVICE,\n" +
+                "    5 - PROJECT_MANAGER,\n" +
+                "    6 - UX_DESIGNER,\n" +
+                "    7 - UI_DESIGNER,\n" +
+                "    8 - BUSINESS_ANALYST,\n" +
+                "    9 - BACKEND_DEVELOPER,\n" +
+                "    10 - FRONTED_DEVELOPER,\n" +
+                "    11 - TESTER_QA,\n" +
+                "    12 - DATA_ANALYST,\n" +
+                "    13 - TEAM_LEAD,\n" +
+                "    14 - TEACH_LEAD,\n" +
+                "    15 - SCRUM_MASTER.\n" +
+                "    <<<";
+        System.out.println(departmentMenu);
+
+    }
 }
